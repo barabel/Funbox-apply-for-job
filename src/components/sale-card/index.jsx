c69712904e0bfb66eb0bc946f17c4b0ccaa5991c
Michail <barabel324@yandex.ru>
2022-06-20 17:52:59 +0400

    styles

--- src/components/sale-card/index.jsx
+++ src/components/sale-card/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import parse from 'html-react-parser';
+import cn from "classnames";
 import './style.scss';
 
 const SaleCard = ({
@@ -8,31 +10,31 @@ const SaleCard = ({
   underTitle,
   description,
   mass,
-  image
+  className
 }) => {
   return (
-    <div className='sale-card'>
-      {typeof aboveTitle === 'string' && <div className='sale-card__above-title'>{aboveTitle}</div>}
-      {typeof title === 'string' && <h3 className='sale-card__title'>{title}</h3>}
-      {typeof underTitle === 'string' && <div className='sale-card__under-title'>{underTitle}</div>}
-      {Array.isArray(description) && (
-        <ul className='sale-card__description'>
-          {
-            description.map((point, index) => {
-              return typeof point === 'string' && <li key={`${index}`} className='sale-card__point'>{point}</li>
-            })
-          }
-        </ul>
-      )}
-      {image && (
-        typeof image.src === 'string' && <img className='sale-card__image' src={image.src} alt={image.alt ? image.alt : 'image'} />
-      )}
-      {mass && (
-        <div className='sale-card__mass'>
-          {typeof mass.value === 'string' && <div className='sale-card__mass-value'>{mass.value}</div>}
-          {typeof mass.units === 'string' && <div className='sale-card__mass-units'>{mass.units}</div>}
-        </div>
-      )}
+    <div className={cn('sale-card', className)}>
+      <div className='sale-card__wrapper'>
+        {typeof aboveTitle === 'string' && <div className='sale-card__above-title'>{aboveTitle}</div>}
+        {typeof title === 'string' && <h3 className='sale-card__title'>{title}</h3>}
+        {typeof underTitle === 'string' && <div className='sale-card__under-title'>{underTitle}</div>}
+        {Array.isArray(description) && (
+          <ul className='sale-card__description'>
+            {
+              description.map((point, index) => {
+                return typeof point === 'string' && <li key={`${index}`} className='sale-card__point'>{parse(point)}</li>
+              })
+            }
+          </ul>
+        )}
+        <img className='sale-card__image' src='/assets/images/cat.png' alt='cat' />
+        {mass && (
+          <div className='sale-card__mass'>
+            {typeof mass.value === 'string' && <div className='sale-card__mass-value'>{mass.value}</div>}
+            {typeof mass.units === 'string' && <div className='sale-card__mass-units'>{mass.units}</div>}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
@@ -46,10 +48,7 @@ SaleCard.propTypes = {
     value: PropTypes.string,
     units: PropTypes.string
   }),
-  image: PropTypes.shape({
-    src: PropTypes.string,
-    alt: PropTypes.string
-  })
+  className: PropTypes.string
 }
 
 export default SaleCard;
--- src/components/sale/index.jsx
+++ src/components/sale/index.jsx
@@ -8,24 +8,24 @@ const Sale = ({
   cards
 }) => {
   return (
-    <section className='sale'>
+    <section className='sale container'>
       {typeof heading === 'string' && <h2 className='sale__heading'>{heading}</h2>}
       {Array.isArray(cards) && (
-        <div className='sale__cards'>
+        <ul className='sale__list'>
           {cards.map((card, index) => {
             return (
-              <div key={`card-${index}`} className='sale__card'>
-                <SaleCard {...card} />
+              <li key={`card-${index}`} className='sale__item'>
+                <SaleCard className={'sale__card'} {...card} />
                 {card.callToAction && (
                   <div className='sale__call-to-action'>
                     {typeof card.callToAction.text === 'string' && <span className='sale__call-to-action-text'>{card.callToAction.text}</span>}
-                    {typeof card.callToAction.action === 'string' && <span className='sale__call-to-action-action'>{card.callToAction.action}</span>}
+                    {typeof card.callToAction.action === 'string' && <span className='sale__call-to-action-action'><b>{card.callToAction.action}</b>.</span>}
                   </div>
                 )}
-              </div>
+              </li>
             );
           })}
-        </div>
+        </ul>
       )}
     </section>
   );
@@ -42,10 +42,7 @@ Sale.propTypes = {
       value: PropTypes.string,
       units: PropTypes.string
     }),
-    image: PropTypes.shape({
-      src: PropTypes.string,
-      alt: PropTypes.string
-    }),
+    className: PropTypes.string,
     callToAction: PropTypes.shape({
       text: PropTypes.string,
       action: PropTypes.string
